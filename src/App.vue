8e7acf5e62f254fe21c50aea0743a534cad2d801
gustavosrosa <g.dsantosrosa@gmail.com>
2025-07-16 12:09:27 -0300

    BKLT: FEAT: Utilizando props reativas com ref()

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <script setup>
 
+import { ref } from 'vue';
+
+// ref para primitivos (números, strings, dentre outros)
+let count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
 </script>
 
 <template>
   <div>
-  
+
+    <h1>{{ count }}</h1>
+    <button @click="increment">Incrementar</button>
+
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
