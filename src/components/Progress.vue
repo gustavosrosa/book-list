bb59099f1088f8feb42eb51ab8606c8c317f89b6
gustavosrosa <g.dsantosrosa@gmail.com>
2025-09-04 15:57:56 -0300

    BKLT: FEAT: Utilizando computed properties

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -1,9 +1,16 @@
 <script setup>
+    import { computed } from 'vue';
+
     const props = defineProps(['courses']);
+
+    const coursesDone = computed(() => {
+        // Precisa usar o props no script, no template nao precisa
+        return props.courses.filter((course) => course.done).length;
+    });
 </script>
 
 <template>
     <div>
-        {{ courses.length }}
+        {{ `${coursesDone} cursos completos de um total de ${courses.length}` }}
     </div>
 </template>
