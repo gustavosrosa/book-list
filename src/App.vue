bcd72110efc555bf9888de7e3723ce85e59a020c
gustavosrosa <g.dsantosrosa@gmail.com>
2025-09-04 16:15:10 -0300

    BKLT: FEAT: Criando estrutura inicial props

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,45 @@
 <script setup>
-let books = [
-  {
-    id: 1,
-    title: "History of Europe",
-    cover:
-      "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-6-540x861.jpg",
-    isRead: true,
-    isbn: "0-395-07157-8",
-    author: "Daniel Trejo",
-  },
-  {
-    id: 2,
-    title: "Penguin Classics",
-    cover:
-      "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-2-540x861.jpg",
-    isRead: false,
-    isbn: "0-395-07157-8",
-    author: "Daniel Trejo, Jon Snow",
-  },
-  {
-    id: 3,
-    title: "Becoming",
-    cover:
-      "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-7-540x861.jpg",
-    isRead: false,
-    isbn: "0-395-07157-8",
-    author: "Daniel Trejo",
-  },
-  {
-    id: 4,
-    title: "Sonnets",
-    cover:
-      "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-5-540x861.jpg",
-    isRead: false,
-    isbn: "0-395-07157-8",
-    author: "Daniel Trejo",
-  },
-]
+
+  import Books from './components/Books.vue';
+
+  let books = [
+    {
+      id: 1,
+      title: "History of Europe",
+      cover:
+        "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-6-540x861.jpg",
+      isRead: true,
+      isbn: "0-395-07157-8",
+      author: "Daniel Trejo",
+    },
+    {
+      id: 2,
+      title: "Penguin Classics",
+      cover:
+        "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-2-540x861.jpg",
+      isRead: false,
+      isbn: "0-395-07157-8",
+      author: "Daniel Trejo, Jon Snow",
+    },
+    {
+      id: 3,
+      title: "Becoming",
+      cover:
+        "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-7-540x861.jpg",
+      isRead: false,
+      isbn: "0-395-07157-8",
+      author: "Daniel Trejo",
+    },
+    {
+      id: 4,
+      title: "Sonnets",
+      cover:
+        "https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-5-540x861.jpg",
+      isRead: false,
+      isbn: "0-395-07157-8",
+      author: "Daniel Trejo",
+    },
+  ]
 </script>
 
 <template>
@@ -49,24 +52,9 @@ let books = [
     </div>
 
     <div class="books-container">
-      <div class="books-list">
-        <div class="book">
-          <div class="book-cover">
-            <img
-              src="https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-7-540x861.jpg" />
 
-            <button>
-              <i class="fa-solid fa-eye"></i>
-              <span>Ainda não li</span>
-            </button>
-          </div>
-          <div class="book-details">
-            <p class="book-author">Daniel Trejo</p>
-            <h3 class="book-title">History of Europe</h3>
-            <p><i class="fa-solid fa-hashtag icon"></i> 0-395-07157-8</p>
-          </div>
-        </div>
-      </div>
+      <Books :books="books"/>
+      
     </div>
   </div>
 </template>
