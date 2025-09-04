68cafa47d85a66e8da59ade019d7945d455e17ca
gustavosrosa <g.dsantosrosa@gmail.com>
2025-09-04 15:42:32 -0300

    BKLT: FEAT: Utilizando props reativas com reactive

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,24 @@
 <script setup>
 
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 // ref para primitivos (números, strings, dentre outros)
 let count = ref(0);
+
+// reactive para objetos (é uma convenção)
+let courses = reactive([
+  {
+    title: "JavaScript"
+  },
+  {
+    title: "React"
+  },
+  {
+    title: "Vue"
+  },
+]);
+
+let newCourse = {};
 
 function increment() {
   count.value++;
@@ -16,6 +31,15 @@ function increment() {
 
     <h1>{{ count }}</h1>
     <button @click="increment">Incrementar</button>
+
+    <ul>
+      <li v-for="course in courses" :key="course.title">{{ course.title }}</li>
+    </ul>
+
+    <br>
+
+    <input type="text" v-model="newCourse.title">
+    <button @click="courses.push(newCourse)">Adicionar</button>
 
   </div>
 </template>
